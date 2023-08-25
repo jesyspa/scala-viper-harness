c8d59ef28965c2375c9b0aa8e9fc3dcbc4c65ce1
Anton Golov <agolov@science.ru.nl>
2023-08-25 16:05:44 +0200

    Trying labels and gotos

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,24 @@
 import viper.silicon.Config
 import viper.silicon.logger.NoopSymbExLog
 import viper.silicon.verifier.DefaultMainVerifier
-import viper.silver.ast.{Div, Inhale, Int, IntLit, LocalVar, LocalVarAssign, LocalVarDecl, Method, NeCmp, NoInfo, NoPosition, NoTrafos, Program, Seqn}
+import viper.silver.ast.{Div, Goto, Inhale, Int, IntLit, Label, LocalVar, LocalVarAssign, LocalVarDecl, Method, NeCmp, NoInfo, NoPosition, NoTrafos, Program, Seqn}
 import viper.silver.reporter.NoopReporter
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val xName = "local$x"
-    val yName = "local$y"
-    val retName = "ret$"
-    val x = LocalVar(xName, Int)()
+    val labelName = "x"
     val myMethod = Method(
-      "global$pkg_$division",
-      Seq(LocalVarDecl(xName, Int)()), /* Parameters */
-      Seq(LocalVarDecl(retName, Int)()), /* Returns */
+      "foo",
+      Seq(), /* Parameters */
+      Seq(), /* Returns */
       Nil, /* Preconditions */
       Nil, /* Postconditions */
       Some(Seqn(
         Seq(
-          Inhale(NeCmp(x, IntLit(0)())())(),
-          LocalVarAssign(LocalVar(yName, Int)(), Div(x, x)())()
+          Label(labelName, Nil)(),
+          Goto(labelName)(),
         ), /* Statements */
-        Seq(
-          LocalVarDecl(yName, Int)()
-        ) /* Declarations */
+        Seq( ) /* Declarations */
       )())
     )()
     val program = Program(
@@ -34,8 +29,10 @@ object Main {
       Seq(myMethod), /* Method */
       Nil, /* Extension */
     )()
+    println(program)
+    println(program.checkTransitively)
     val verifier = new DefaultMainVerifier(new Config(Nil), NoopReporter, NoopSymbExLog)
     verifier.start()
-    verifier.verify(program, Nil, None)
+    println(verifier.verify(program, Nil, None))
   }
 }
